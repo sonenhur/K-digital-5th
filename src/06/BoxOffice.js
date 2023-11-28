3dcdeb02297803386b8a6c8dbad4479086199496
sonenhur <sonen1125@gmail.com>
2023-11-28 13:05:36 +0900

    20231128

--- src/06/BoxOffice.js
+++ src/06/BoxOffice.js
@@ -4,8 +4,8 @@ import { BiCameraMovie } from "react-icons/bi";
 export default function BoxOffice() {
     return (
         <div className="container mx-auto bg-white h-screen">
-            <div className="flex flex-col justify-center items-center h-full">
-                <div className="m-8"><BiCameraMovie className="text-4xl text-gray-800" />
+            <div className="flex flex-col justify-center items-center w-full h-full">
+                <div className="flex m-8"><BiCameraMovie className="text-4xl text-gray-800" />
                     <TailH1 title="박스오피스" />
                 </div>
                 <div className="relative overflow-x-auto w-3/4 shadow-md sm:rounded-lg">
