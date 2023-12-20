5b7ab7f496753105ed6811e8ee247f965db9985b
sonenhur <sonen1125@gmail.com>
2023-12-20 11:29:51 +0900

    20231220

--- src/10/Gallery.js
+++ src/10/Gallery.js
@@ -2,52 +2,77 @@ import React, { useEffect, useRef, useState } from 'react';
 import TailH1 from '../UI/TailH1';
 import TailButton from '../UI/TailButton';
 import TailCard from '../UI/TailCard';
-import { FcLandscape } from "react-icons/fc";
-import { FcNightLandscape } from "react-icons/fc";
+import { FcLandscape, FcNightLandscape } from "react-icons/fc";
+
+const apikey = process.env.REACT_APP_APIKEY;
 
 export default function Gallery() {
-    // const [tdata, setTdata] = useState();
-    // 환경변수값 가져오기
-    let apikey = process.env.REACT_APP_APIKEY;
-
-    //fetch 데이터 저장
-    const [tdata, setTdata] = useState();
-
-    //키워드 입력
+    // 키워드 입력
     const kwInput = useRef();
 
+    // 태그 상태 변수 정의와 초기화
+    const [tags, setTags] = useState([]);
+
+    // tdata 상태 변수 정의와 초기화
+    const [tdata, setTData] = useState([]);
+
+    // 키워드 입력 이벤트 처리
     const handleGetData = async (e) => {
         if (e.key !== "Enter") e.preventDefault();
-        //키워드 인코딩
+        // 키워드 인코딩
         let enkw = encodeURI(kwInput.current.value);
         if (enkw === '') {
-            alert('키워드를 입력하세요')
+            alert('키워드를 입력하세요');
             kwInput.current.focus();
             return;
         }
-        let url = `https://apis.data.go.kr/B551011/PhotoGalleryService1/gallerySearchList1?`
-        url = url + `serviceKey=${apikey}`
-        url = url + `&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=AppTest&arrange=A`
-        url = url + `&keyword=${enkw}`
-        url = url + `&_type=json`;
 
+        let url = `https://apis.data.go.kr/B551011/PhotoGalleryService1/gallerySearchList1?`;
+        url = `${url}serviceKey=${apikey}`
+        url = `${url}&numOfRows=10`;
+        url = `${url}&pageNo=1`;
+        url = `${url}&MobileOS=ETC`;
+        url = `${url}&MobileApp=AppTest`;
+        url = `${url}&arrange=A`;
+        url = `${url}&keyword=${enkw}`;
+        url = `${url}&_type=json`;
 
-        const resp = await fetch(url);
-        const data = await resp.json();
+        try {
+            const resp = await fetch(url);
+            const data = await resp.json();
 
-        setTdata(data.response.body.items.item);
-    };
+            if (data.response && data.response.body && data.response.body.items && data.response.body.items.item) {
+                setTData(data.response.body.items.item);
+            } else {
+                alert('데이터를 불러오는데 문제가 발생했습니다.');
+            }
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
 
+    // 데이터 초기화 이벤트 처리
     const handleResetData = (e) => {
         e.preventDefault();
         kwInput.current.value = '';
+        setTags([]); // 입력값 초기화 시 태그도 초기화
     };
 
-    // tdata변경시
     useEffect(() => {
-        console.log("tdata = ", tdata)
+        // tdata를 사용하여 태그 업데이트 로직 추가
+        let tm = tdata.map((item, idx) => (
+            <TailCard
+                key={`card${idx}`}
+                imgSrc={item.galWebImageUrl.replace('http://', 'https://')}
+                title={item.galTitle}
+                subtitle={item.galPhotographyLocation}
+                tags={item.galSearchKeyword}
+            />
+        ));
 
-    }, [tdata])
+        // 태그 업데이트
+        setTags(tm);
+    }, [tdata]);
 
     return (
         <div className="container mx-auto h-screen">
@@ -60,23 +85,19 @@ export default function Gallery() {
                 <form name="kform" className="mt-10 my-8 w-4/5 flex justify-center items-center">
                     <div className="w-fit flex items-center border-b border-teal-500">
                         <input
+                            type="text"
                             ref={kwInput}
                             className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
-                            type="text"
                             placeholder="키워드 입력"
+                            required
                         />
-                        <TailButton caption='확인' bcolor='teal' handleClick={(e) => handleGetData(e)} />
-                        <TailButton caption='취소' bcolor='transparent' handleClick={(e) => handleResetData(e)} />
+                        <TailButton caption='확 인' bcolor='teal' handleClick={(e) => handleGetData(e)} />
+                        <TailButton caption='취 소' bcolor='transparent' handleClick={(e) => handleResetData(e)} />
                     </div>
                 </form>
             </div>
-            <div className="flex justify-center items-center">
-                <TailCard
-                    imgSrc={"https://tong.visitkorea.or.kr/cms2/website/61/2952361.jpg"}
-                    title={"광안리해수욕장"}
-                    subtitle={"부산광역시 수영구 광안동"}
-                    tags={"광안리해수욕장, 부산광역시 수영구, 광안리해변, 바닷가, 바다, 부산 광안대교, 다이아몬드 브릿지, 별바다부산, 부산야간관광"}
-                />
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                {tags}
             </div>
         </div>
     );
--- src/UI/TailCard.js
+++ src/UI/TailCard.js
@@ -1,20 +1,25 @@
-import React from 'react';
+export default function TailCard({ imgSrc, title, subtitle, tags }) {
+    let sps = tags.split(',');
+    sps = sps.map((item, idx) =>
+        <span key={`sp${idx}`} className="inline-block bg-teal-600 rounded-full px-3 py-1 text-white text-sm font-medium mr-2 mb-2">
+            {item}
+        </span>
+    )
 
-export default function TailCard(props) {
-    const { imgSrc, title, subtitle, tags } = props;
+    sps = sps.length === 0 ? '' : sps
 
     return (
         <div className="max-w-lg overflow-hidden shadow-lg bg-teal-200 rounded-3xl text-teal-600">
             <img className="w-full" src={imgSrc} alt={title} />
             <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">{title}</div>
-                <p className="font-medium text-teal-600">{subtitle}</p>
+                <div className="font-bold text-xl mb-2">
+                    {title}
+                </div>
+                <p className="font-medium text-teal-600">
+                    {subtitle}
+                </p>
                 <div className="mt-4">
-                    {tags.split(', ').map((tag, index) => (
-                        <span key={index} className="inline-block bg-teal-600 rounded-full px-3 py-1 text-white text-sm font-medium mr-2 mb-2">
-                            {tag}
-                        </span>
-                    ))}
+                    {sps}
                 </div>
             </div>
         </div>
