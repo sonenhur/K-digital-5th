1757998a68ca1bb14f592e88add5a84ed7b15b10
sonenhur <sonen1125@gmail.com>
2024-01-10 17:55:03 +0900

    20240110

--- src/Component/SubwayInfo.js
+++ src/Component/SubwayInfo.js
@@ -5,6 +5,7 @@ export default function SubwayInfo() {
   const apikey = process.env.REACT_APP_API_KEY;
   const [ops, setOps] = useState([]);
   const [airQualityData, setAirQualityData] = useState(null);
+  const [tags, setTags] = useState([]);
 
   const getData = async (cd) => {
     let url = `https://apis.data.go.kr/6260000/IndoorAirQuality/getIndoorAirQualityByStation`
@@ -15,36 +16,52 @@ export default function SubwayInfo() {
     url = url + `&controlnumber=2024010918`
     url = url + `&areaIndex=${cd}`
 
-      const resp = await fetch(url);
-      const data = await resp.json();
-      setAirQualityData(data.getIndoorAirQualityByStation.body.items.item[0]);
-    
+    const resp = await fetch(url);
+    const data = await resp.json();
+    setAirQualityData(data.getIndoorAirQualityByStation.body.items.item[0]);
   }
 
   const handleSelectArea = (e) => {
     getData(e.target.value);
+    console.log(e.target.value)
   }
 
   useEffect(() => {
-    let tm = SubwayArea.map((item, idx) => (
+    if (airQualityData === null) return;
+
+    const k = ['co2', 'co', 'no2', 'no', 'nox', 'o3', 'pm25', 'fad'];
+
+    let tm = k.map((item, idx) => (
+      <div className="flex flex-col" key={item}>
+        <div>{item}</div>
+        <div>{airQualityData[item]}</div>
+      </div>
+    ));
+
+    setTags(tm);
+    console.log(tm)
+  }, [airQualityData]);
+
+  useEffect(() => {
+    // Corrected mapping and setting options
+    const options = SubwayArea.map((item, idx) => (
       <option key={idx} value={item['코드']}>
         {item['측정소']}
       </option>
     ));
-    setOps(tm);
+    setOps(options);
   }, []);
 
   return (
     <div className='w-1/4 h-full flex flex-col items-center mt-40'>
-      <div >
+      <div>
         대기정보확인 지역선택
       </div>
       <div className="w-full">
         <select
           onChange={handleSelectArea}
           id="countries"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-        >
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" >
           <option value="">---지역 선택---</option>
           {ops}
         </select>
@@ -52,7 +69,7 @@ export default function SubwayInfo() {
 
       {airQualityData && (
         <div>
-          <h2>Air Quality Data:</h2>
+          <h2>대기질 정보:</h2>
           <table>
             <tbody>
               {Object.entries(airQualityData).map(([key, value]) => (
