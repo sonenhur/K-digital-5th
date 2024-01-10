1f378d4cb240325608433f127b5495e983df3c2c
sonenhur <sonen1125@gmail.com>
2024-01-10 17:38:49 +0900

    20240110

--- src/Component/Subway.js
+++ src/Component/Subway.js
@@ -5,7 +5,7 @@ import Login from "./Login";
 export default function Subway() {
     const isLogin = useRecoilValue(stLogin);
     return (
-        <div className='w-full h-full flex flex-col items-center justify-center'>
+        <div className='w-full h-full flex flex-col items-center mt-40'>
             {isLogin ? <SubwayInfo /> : <Login />}
         </div>
     )
--- src/Component/SubwayInfo.js
+++ src/Component/SubwayInfo.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
-import SubwayArea from '../DB/SubwayArea.json'
+import SubwayArea from '../DB/SubwayArea.json';
+
 export default function SubwayInfo() {
   const apikey = process.env.REACT_APP_API_KEY;
   const [ops, setOps] = useState([]);
+  const [airQualityData, setAirQualityData] = useState(null);
 
   const getData = async (cd) => {
     let url = `https://apis.data.go.kr/6260000/IndoorAirQuality/getIndoorAirQualityByStation`
@@ -13,41 +15,56 @@ export default function SubwayInfo() {
     url = url + `&controlnumber=2024010918`
     url = url + `&areaIndex=${cd}`
 
-    const resp = await fetch(url);
-    const data = await resp.json();
-
-    console.log(data.getIndoorAirQualityByStation.body.items.item[0])
+      const resp = await fetch(url);
+      const data = await resp.json();
+      setAirQualityData(data.getIndoorAirQualityByStation.body.items.item[0]);
+    
   }
-  const  handleSelectArea = (e)=>{
-    console.log(e.target.value)
+
+  const handleSelectArea = (e) => {
     getData(e.target.value);
   }
+
   useEffect(() => {
-    console.log(SubwayArea)
     let tm = SubwayArea.map((item, idx) => (
       <option key={idx} value={item['코드']}>
         {item['측정소']}
       </option>
     ));
-    setOps(tm)
-   
-  }, [])
+    setOps(tm);
+  }, []);
 
   return (
-    <div>
-      <div className='w-full h-full flex flex-col items-center justify-center'>
+    <div className='w-1/4 h-full flex flex-col items-center mt-40'>
+      <div >
         대기정보확인 지역선택
       </div>
       <div className="w-full">
-        <select onChange={handleSelectArea}
+        <select
+          onChange={handleSelectArea}
           id="countries"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        >
           <option value="">---지역 선택---</option>
           {ops}
         </select>
       </div>
-      <div className="grid-cols-1 md:grid-cols-3 ">자료</div>
+
+      {airQualityData && (
+        <div>
+          <h2>Air Quality Data:</h2>
+          <table>
+            <tbody>
+              {Object.entries(airQualityData).map(([key, value]) => (
+                <tr key={key}>
+                  <td>{key}</td>
+                  <td>{value}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
-  
 }
